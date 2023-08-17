249b1631b272d6ec5f678784e1672b4adfcaf10f
Hector <quimat2@hotmail.com>
2023-08-17 08:45:31 -0600

    agregamos cowsay

---
+++ app.js
@@ -0,0 +1,46 @@
+/*
+
+var uno = 2
+var uno = "hola"
+
+let dos = 9
+let dos = 'hola' //Marca error
+
+const tres = 0
+
+function holamundo(uno, dos) {
+
+}
+
+const holaMundo = (uno, dos) => {
+
+}
+
+uno = [1, 2, 3, 4, 5]
+
+for (let i = 0; i < 5; i++) {
+    uno[i]
+}
+
+let i = 0
+do {
+
+} while (i < 5)
+
+uno.array.forEach(element => {
+    console.log(item)
+});
+
+let objeto = {
+    valor1: 0,
+    valor2: 1
+}
+
+*/
+const { frutas, money } = require('./frutas')
+
+frutas.forEach(fruta => {
+    console.log('La fruta es: ', fruta)
+})
+
+console.log('Dinero: ', money)
---
+++ frutas.js
@@ -0,0 +1,14 @@
+const frutas = ['pera', 'fresa', 'manzana', 'mango', 'papaya']
+const money = 5000
+
+module.exports = {
+    //Manera antigua
+    /*
+    frutas: frutas,
+    money
+    */
+
+    //Manera moderna
+    frutas,
+    money
+};
